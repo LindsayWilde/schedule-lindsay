50e17107fa94f1f428fe353790d222887f7ef12a
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-16 10:51:20 -0600

    libraryCourse component and styled a bit with grid

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem solving</label>
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut accumsan nisi. Nunc diam dui, malesuada sed consequat in, ullamcorper non massa. Aenean quis orci vitae justo pulvinar </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LibraryCourse;
