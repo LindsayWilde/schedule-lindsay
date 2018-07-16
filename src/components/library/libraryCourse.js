c4b72289dc2df924ea4922fa1186c5d33fc80ad5
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-16 11:44:38 -0600

    libray course gird styled/finished

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+
 import Icon from '../icon';
 import Arrow from '../arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <div className="library-course__title-arrow">
+                <div className="library-course__title-check">
                     <label className="library-course__title">Problem solving</label>
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <Arrow className='library-course__arrow'/>
+                <Action className="library-course__action" />
+
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut accumsan nisi. Nunc diam dui, malesuada sed consequat in, ullamcorper non massa. Aenean quis orci vitae justo pulvinar </p>
