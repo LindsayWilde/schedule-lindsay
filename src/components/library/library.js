f29947424e7aee8bb8851519396bf53bc9c65050
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-17 11:26:29 -0600

    implemented mapStateToProps function; mapped over data

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -5,21 +5,32 @@ import * as actions from '../../actions';
 import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
+
+    componentWillMount() {
+        this.props.fetchCourses()
+    }
+
+    renderCouses() {
+        const data = this.props.courses
+        return data.map((course, index) => {
+            return <LibraryCourse key={index}/>
+        })
+    }
+
     render() {
-
-        function componentDidMount() {
-            this.props.fetchCourses()
-        }
-
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
-                <LibraryCourse />
-                <LibraryCourse />
-                <LibraryCourse />
+                {this.renderCouses()}
             </div>
         )
     }
 }
 
-export default connect(null, actions)(Library);
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
