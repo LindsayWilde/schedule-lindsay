170232e831fb6bac37153b172e6ec58dd1c1d869
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-19 11:08:58 -0600

    styled schedule component and redid schedule grid template rows

--- src/components/action.js
+++ src/components/action.js
@@ -11,7 +11,6 @@ class Action extends Component {
 
     handleAction = function() {
         this.props.onClick()
-
         if(!this.status) {
             document.getElementById(this.id).classList.add('action-remove');
         } else {
@@ -27,15 +26,8 @@ class Action extends Component {
                 id={this.id}
                 onClick={() => this.handleAction()}
                 className={`${this.props.className} action`}>
-
             </a>
         )
-        // return (
-        //     <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-        //         { Icon('fas fa-plus-circle') }
-        //         {/* fas fa-times-circle */}
-        //     </button>
-        // )
     }
 }
 
